16b619167cba8be3f75fc5dbd7aca4c43af6d896
fateme <abedifateme1378@gmail.com>
2022-09-11 17:16:20 +0430

    project finished

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, createTheme, ThemeProvider } from '@mui/material';
+// import Button from '@mui/material/Button';
+import { Stack } from '@mui/system'
+import Sidebar from './Components/Sidebar'
+import Feed from './Components/Feed'
+import Rightbar from './Components/Rightbar'
+import Navbar from './Components/Navbar'
+import AddPost from './Components/AddPost'
+import React,{useState} from 'react'
 
 function App() {
+const [mode,setMode]=useState('dark');
+
+  const darkTheme=createTheme({
+    palette:{
+     mode:mode
+    }
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+     
+    
+    <Box bgcolor={"background.default"} color={"text.primary"}>
+      <Navbar/>
+      <Stack direction='row' spacing={2} justifyContent={'space-between'}>
+        <Sidebar setMode={setMode} mode={mode}/>
+        
+        <Feed/>
+        <Rightbar/>
+      </Stack>
+     <AddPost/>
+    </Box>
+    </ThemeProvider>
   );
 }
 
